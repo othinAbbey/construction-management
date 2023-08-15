eceb3f41c5a4bcf0a40cd74ef18782295d0a35e3
othinAbbey <othincore@gmail.com>
2023-08-15 11:47:36 +0300

    Import assest

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+// import "./App.css";
+import { useForm } from "@mantine/form";
+import { NumberInput, TextInput, Button, Box } from "@mantine/core";
+// import Navbars from "./componenets/navigation";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  // const [data, setData] = useState({
+  //   name: "",
+  //   email: "",
+  //   age: 0,
+  // });
+  // function handleChanges(event) {
+  //   const key = event.target.name;
+  //   const value = event.target.value;
+  //   setData({
+  //     ...data,
+  //     [key]: value,
+  //   });
+  // }
+  // // const handleChanges = (event) => {
+  // //   const { key, value } = event.target;
+  // //   setData({
+  // //     ...data,
+  // //     [key]: value,
+  // //   });
+  // // };
+  // // const form = useForm({
+  // //   initialValues: { name: "", email: "", age: 0 },
+  // //   // functions will be used to validate values at corresponding key
+  // //   // validate: {
+  // //   //   name: (value) =>
+  // //   //     value.length < 2 ? "Name must have at least 2 letters" : null,
+  // //   //   email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+  // //   //   age: (value) =>
+  // //   //     value < 18 ? "You must be at least 18 to register" : null,
+  // //   // },
+  // // });
+  // return (
+  //   <Box maw={320} mx="auto">
+  //     <form>
+  //       <TextInput label="Name" placeholder="Name" onChange={handleChanges} />
+  //       <TextInput
+  //         mt="sm"
+  //         label="Email"
+  //         placeholder="Email"
+  //         onChange={handleChanges}
+  //       />
+  //       <NumberInput
+  //         mt="sm"
+  //         label="Age"
+  //         placeholder="Age"
+  //         min={0}
+  //         max={99}
+  //         onChange={handleChanges}
+  //       />
+  //       <Button type="submit" mt="sm">
+  //         Submit
+  //       </Button>
+  //     </form>
+  //   </Box>
+  // );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import DoubleNavbar from "./componenets/Navbar.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <DoubleNavbar />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
