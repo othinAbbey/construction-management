11d802514d38ff3ecced58363ed2afbd771f454e
othinAbbey <othincore@gmail.com>
2023-08-16 17:56:41 +0300

    Add icone componet

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-// import { useState } from "react";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+const AnotherComponent = () => {
+  return (
+    <div>
+      {icons.map((icon, index) => (
+        <img
+          key={index}
+          src={icon.path}
+          alt={icon.name}
+          width="32"
+          height="32"
+        />
+      ))}
+    </div>
+  );
+};
 
-// // import "./App.css";
-// import { useForm } from "@mantine/form";
-// import { NumberInput, TextInput, Button, Box } from "@mantine/core";
-// import Home from "./components/Home.jsx";
-// // import { Navbar } from "@mantine/core";
-// import DoubleNavbar from "./components/Navbar.jsx";
-// // import Navbars from "./componenets/navigation";
+export default AnotherComponent;
+This example assumes that you have SVG icon files stored in an icons folder within your project directory. You would replace './icons/icon1.svg', './icons/icon2.svg', etc., with the actual paths to your icon files.
 
-// function App() {
-//   return (
+By creating an array of icons and importing them into other components, you can easily manage and use icons throughout your React application. This approach allows you to abstract away the details of individual icon paths and manage them more conveniently.
 
-//   )
-// }
 
-// export default App;
+
+
+
