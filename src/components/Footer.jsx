7c1ff5fe9e98e7f264767458794640e8a0f37976
othinAbbey <othincore@gmail.com>
2023-09-02 02:18:42 +0300

    Modify footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,10 @@ import { useClickOutside } from "@mantine/hooks";
 function Footer() {
   return (
     <footer>
-      <h3>C & A Contractors @2023 </h3>
+      <h6>
+        {" "}
+        <em> C & A Contractors @2023 </em>{" "}
+      </h6>
       <h6>
         <em>Site Simplify</em>
       </h6>
