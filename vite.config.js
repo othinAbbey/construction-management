2c2efd1ddc41a0d4a06d90907c4dcc5acdc00169
othinAbbey <othincore@gmail.com>
2023-09-11 16:19:38 +0300

    Correct Vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-// import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 
-// import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
 
 
-const path = require('path')
+// const path = require('path')
 
-export default {
-  root: path.resolve(__dirname, 'src'),
-  build: {
-    outDir: '../dist'
-  },
-  server: {
-    port: 8080
-  }
-}
+// export default {
+//   root: path.resolve(__dirname, 'src'),
+//   build: {
+//     outDir: '../dist'
+//   },
+//   server: {
+//     port: 8080
+//   }
+// }
