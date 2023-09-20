47180d04aa5397d3545f322b0d7bf105e30a6a49
othinAbbey <othincore@gmail.com>
2023-09-20 19:46:47 +0300

    Works

--- src/components/Estimation/Excavation/Excavation.jsx
+++ src/components/Estimation/Excavation/Excavation.jsx
@@ -1,6 +1,65 @@
 import React from "react";
-
+import MyTable from "../../Totals/Tables";
 function Excavation() {
+  const dimensions = [
+    {
+      name: "length",
+      label: "Length (m)",
+      type: "number",
+    },
+    {
+      name: "width",
+      label: "Width (m)",
+      type: "number",
+    },
+    {
+      name: "depth",
+      label: "Depth (m)",
+      type: "number",
+    },
+  ];
+  const excavationTrips = [
+    {
+      name: "Quantity",
+      label: "Quantity",
+      type: "number",
+    },
+
+    {
+      name: "tripVolume",
+      label: "1 Truck Volume",
+      type: "number",
+    },
+    {
+      name: "unitRateExcavation",
+      label: "Unit Price For Excavation",
+      type: "number",
+    },
+    {
+      name: "unitRateTrip",
+      label: "Unit Transport Rate",
+      type: "number",
+    },
+  ];
+  const totals = [
+    {
+      name: "totalExcavation",
+      label: "Total Volume",
+      type: "number",
+    },
+    {
+      name: "totalTrip",
+      label: "Total Trips",
+      type: "number",
+    },
+
+    {
+      name: "totalCost",
+      label: "Total Cost",
+      type: "number",
+    },
+  ];
+
   return (
     <div>
       <div>
@@ -14,85 +73,43 @@ function Excavation() {
           <h1 className="text-3xl font-semibold mb-4">Excavation </h1>
         </div>
       </div>
-      <div className="bg-green-500 p-6 rounded-lg shadow-md flex flex-col sm:flex-row items-center">
-        <div className="bg-blue-500 p-6 rounded-lg shadow-md">
-          <h2> Ground Dimensions </h2>
-          <div className="flex flex-col">
-            <label htmlFor="length" className="text-sm font-medium mb-2">
-              Length (m)
-            </label>
-            <input
-              type="number"
-              id="length"
-              className="border border-gray-300 rounded-lg py-2 px-3"
-            />
-            <label htmlFor="width" className="text-sm font-medium mb-2">
-              Width (m)
-            </label>
-            <input
-              type="number"
-              id="width"
-              className="border border-gray-300 rounded-lg py-2 px-3"
-            />
-            <label htmlFor="depth" className="text-sm font-medium mb-2">
-              Depth (m)
-            </label>
-            <input
-              type="number"
-              id="depth"
-              className="border border-gray-300 rounded-lg py-2 px-3"
-            />
-          </div>
+      <div className=" p-6 rounded-lg shadow-md flex flex-col sm:flex-row p-0 m-0">
+        <div className=" p-6 rounded-lg shadow-md mr-3 p-2">
+          <h2>Ground Dimensions:</h2>
+          {dimensions.map((dimension, index) => (
+            <div className="flex flex-col" key={index}>
+              <label
+                htmlFor={dimension.name}
+                className="text-sm font-medium mb-2"
+              >
+                {dimension.label}
+              </label>
+              <input
+                type={dimension.type}
+                id={dimension.name}
+                className="border border-gray-300 rounded py-1 px-2 h-5"
+              />
+            </div>
+          ))}
         </div>
-        <div className="bg-gray-500 p-6 rounded-lg shadow-md">
-          <h2>Exacavation Quantity & Trip Voulume</h2>
-          <div className="flex flex-col">
-            <label
-              htmlFor="excavationQuantity"
-              className="text-sm font-medium mb-2"
-            >
-              Excavation Quantity
-            </label>
-            <input
-              type="number"
-              id="excavationQuantity"
-              className="border border-gray-300 rounded-lg py-2 px-3"
-            />
-            <label htmlFor="tripVolume" className="text-sm font-medium mb-2">
-              Trip Volume
-            </label>
-            <input
-              type="number"
-              id="tripVolume"
-              className="border border-gray-300 rounded-lg py-2 px-3"
-            />
-
-            <label htmlFor="tripVolume" className="text-sm font-medium mb-2">
-              1 Trip Volume
-            </label>
-            <input
-              type="number"
-              id="tripVolume"
-              className="border border-gray-300 rounded-lg py-2 px-3"
-            />
-
-            <label htmlFor="price" className="text-sm font-medium mb-2">
-              Unit Rate Excavation
-            </label>
-            <input
-              type="number"
-              id="price"
-              className="border border-gray-300 rounded-lg py-2 px-3"
-            />
-            <label htmlFor="price" className="text-sm font-medium mb-2">
-              Unit Rate Trip
-            </label>
-            <input
-              type="number"
-              id="price"
-              className="border border-gray-300 rounded-lg py-2 px-3"
-            />
-          </div>
+        <div className=" p-6 rounded-lg shadow-md p-2">
+          <h2>Excavation Quantity & Trip Volumn:</h2>
+          {excavationTrips.map((trip, index) => (
+            <div className="flex flex-col" key={index}>
+              <label htmlFor={trip.name} className="text-sm font-medium mb-2">
+                {trip.label}
+              </label>
+              <input
+                key={index}
+                type={trip.type}
+                id={trip.name}
+                className="border border-gray-300 rounded py-1 px-2 h-5"
+              />
+            </div>
+          ))}
+        </div>
+        <div className="max-w-full max-h-[400px] overflow-auto">
+          <MyTable />
         </div>
       </div>
     </div>
