f7d5ed618feb706e46702e701181ab24f5cd249d
othinAbbey <othincore@gmail.com>
2023-10-06 22:58:04 +0300

    Delete all files and restart with Next.js

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,42 +1,34 @@
-import React, { useState } from "react";
+// Home.jsx
+
+import React from "react";
 import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import ActionCard from "../components/Home/ActionCard.jsx";
+import "./home.css"; // Make sure the path to your CSS file is correct
+import Slider from "react-slick";
 
 function Home() {
-  const [showFullDescription, setShowFullDescription] = useState(false);
-
-  const toggleFullDescription = (index) => {
-    const updatedCardsData = [...actionCardsData];
-    updatedCardsData[index].showFullDescription =
-      !updatedCardsData[index].showFullDescription;
-    setActionCardsData(updatedCardsData);
-  };
-
   const actionCardsData = [
     {
       icon: (
         <img
           src="../src/assets/Image/Bricks.jpg"
           alt="Materials"
-          // className="max-w-xs"
-          class="sm:w-10 sm:h-10 md:w-10 md:h-10 lg:w-10 lg:h-10 xl:w-10 xl:h-10"
+          className="icon"
         />
       ),
       title: "Find Materials",
       shortDescription:
-        "Locate the essential materials required for your upcountry contracts. ",
+        "Locate the essential materials required for your upcountry contracts.",
       fullDescription:
-        "Locate the essential materials required for your upcountry contracts.  As a contractor working in remote areas, sourcing the right materials is crucial for your success. Navigate through a variety of options and access quality materials to ensure the seamless execution of your projects.",
-      showFullDescription: false,
+        "Locate the essential materials required for your upcountry contracts. As a contractor working in remote areas, sourcing the right materials is crucial for your success. Navigate through a variety of options and access quality materials to ensure the seamless execution of your projects.",
     },
     {
       icon: <UserCircleIcon />,
       title: "Calculate Materials Quantity",
       shortDescription:
-        "Effortlessly estimate the quantity of materials needed for your projects. ",
+        "Effortlessly estimate the quantity of materials needed for your projects.",
       fullDescription:
         "Effortlessly estimate the quantity of materials needed for your projects. Avoid over-purchasing or running short on supplies. Accurate calculations save you time and money, ensuring your construction or renovation work progresses smoothly.",
-      showFullDescription: false,
     },
     {
       icon: <PhotoIcon />,
@@ -45,15 +37,13 @@ function Home() {
         "Streamline your budget planning by calculating labor costs for your projects.",
       fullDescription:
         "Streamline your budget planning by calculating labor costs for your projects. Account for wages, hours worked, and project timelines. Make informed decisions, optimize resources, and keep your projects within budget.",
-      showFullDescription: false,
     },
     {
       icon: <PhotoIcon />,
       title: "How It Works",
-      shortDescription: "Discover the simplicity of our application. ",
+      shortDescription: "Discover the simplicity of our application.",
       fullDescription:
         "Discover the simplicity of our application. Efficiently manage your construction projects from start to finish. Explore our user-friendly features and step-by-step guides to master the tool and enhance your project management skills.",
-      showFullDescription: false,
     },
     {
       icon: <UserCircleIcon />,
@@ -61,7 +51,6 @@ function Home() {
       shortDescription: "Initiate a new project with confidence.",
       fullDescription:
         "Initiate a new project with confidence. Get started on your next endeavor, plan your tasks, and bring your ideas to life with ease. Collaborate, organize, and innovate as you embark on your project journey. Your success story begins here.",
-      showFullDescription: false,
     },
     {
       icon: <PhotoIcon />,
@@ -70,7 +59,6 @@ function Home() {
         "Manage your projects with ease. Track your progress, monitor your budget, and stay on schedule.",
       fullDescription:
         "Manage your projects with ease. Track your progress, monitor your budget, and stay on schedule. Keep your team informed and engaged. Make informed decisions and ensure your projects are completed on time and within budget.",
-      showFullDescription: false,
     },
     {
       icon: <PhotoIcon />,
@@ -79,9 +67,27 @@ function Home() {
         "Stay in control of your construction projects by keeping a close watch on weather and site conditions.",
       fullDescription:
         "Stay in control of your construction projects by keeping a close watch on weather and site conditions. This comprehensive feature offers you the tools you need to effectively manage environmental factors and site-specific challenges. With real-time weather updates and site condition documentation, you can create a visual history of your project's progress and address potential challenges proactively.",
-      showFullDescription: false,
     },
   ];
+
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  };
+
+  // Slice the actionCardsData array to display only the first 5 cards
+  const displayedActionCards = actionCardsData.slice(0, 5);
 
   return (
     <div className="home">
@@ -94,21 +100,30 @@ function Home() {
       </p>
       <br />
       <div className="Material-List">
-        {actionCardsData.map((cardData, index) => (
+        {displayedActionCards.map((cardData, index) => (
           <ActionCard
             key={index}
             icon={cardData.icon}
             title={cardData.title}
             shortDescription={cardData.shortDescription}
             fullDescription={cardData.fullDescription}
-            // showFullDescription={cardData.showFullDescription}
           />
         ))}
-        {/* <button onClick={toggleFullDescription}>
-          {showFullDescription ? "Read Less" : "Read More"}
-        </button> */}
       </div>
+      {/* Use the Slider component for the remaining action cards */}
+      <Slider {...sliderSettings}>
+        {actionCardsData.slice(5).map((cardData, index) => (
+          <ActionCard
+            key={index + 5} // Add an offset to avoid key conflicts
+            icon={cardData.icon}
+            title={cardData.title}
+            shortDescription={cardData.shortDescription}
+            fullDescription={cardData.fullDescription}
+          />
+        ))}
+      </Slider>
     </div>
   );
 }
+
 export default Home;
