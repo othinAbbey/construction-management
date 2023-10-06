05e22a8d6372d11b74dbe7f3357c2331eaff8da0
othinAbbey <othincore@gmail.com>
2023-10-06 23:00:14 +0300

    all files deleted

--- src/components/adverts.jsx
+++
@@ -1,8 +0,0 @@
-function Adverts() {
-  return (
-    <div className="relative text-blue-500 bg-gray-800 bg-opacity-500 w-full h-auto sm:w-1/5 rounded-lg overflow-hidden mx-auto">
-      <h6>Adverts</h6>
-    </div>
-  );
-}
-export default Adverts;
