1827cc9321a0723129deafe420ae4dc9b1a4ab1b
othinAbbey <othincore@gmail.com>
2023-10-22 16:53:04 +0300

    Materials

--- pages/Materials.jsx
+++ pages/Materials.jsx
@@ -1,10 +1,30 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router"; // Import useRouter to access the router
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 function Materials() {
+  const regions = [
+    { value: '-- Select Region --' },
+    { value: 'Western' }, 
+    { value: 'Northern' },    
+    { value: 'West Nile' },
+    { value: 'Eastern' }
+  ]
+  
+  const [selected, setSelected] = useState(regions[0].value);
+  
+  const handleChange = (e) => {
+    setSelected(e.target.value);
+    console.log(e.target.value);
+  }
+  
   return (
     <div className="mt-16">
-      <h3>Materials</h3>
+      <h3>Search for Materials</h3>
+      <select value={selected} onChange={handleChange}>
+        {regions.map((region) => (
+          <option value={region.value}>{region.value}</option>
+        ))}
+      </select>
     </div>
   );
 }
