4dc9cbbb4be386f0d65b24cb9a29da25f4a0cb40
othinAbbey <othincore@gmail.com>
2023-10-24 11:31:15 +0300

    Footer size

--- pages/Footer.jsx
+++ pages/Footer.jsx
@@ -2,7 +2,7 @@
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-4 text-center  w-full">
+    <footer className="bg-gray-800 text-white py-1 text-center  w-full">
       <div className="container mx-auto">
         <p>
           &copy; {new Date().getFullYear()} C & A Contractors Limited. All
