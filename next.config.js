0172be527682de5933a48da2780f852040cf9aed
othinAbbey <othincore@gmail.com>
2024-02-17 01:48:27 +0300

    nextjsConfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack5: true,
+  };
+  
+  module.exports = nextConfig;
+  
