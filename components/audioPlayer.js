ed1bffbcff431003e0bcc35d60fbb8268e80e127
othinAbbey <othincore@gmail.com>
2024-02-16 15:00:08 +0300

    audio

--- components/audioPlayer.js
+++ components/audioPlayer.js
@@ -1,55 +1,3 @@
-// import React, { useRef, useEffect ,useState} from "react";
-
-// export default function AudioPlayer(props) {
-// const [isPlaying, setIsPlaying] = useState(false); // State to track audio play/pause
-//   const audio = useRef(null);
-
-//   const playPromise = audio.current.play();
-
-//   useEffect(() => {
-//     if (props.isPlaying) {
-//       setIsPlaying(false);
-//     } else {
-//        setIsPlaying(true);
-//     }
-//   }, [props.play]);// Update the state when the play prop changes
-
-
-//   if (playPromise !== undefined) {
-//     playPromise
-//       .then((_) => {
-
-//         // Automatic playback started!
-//         // Show playing UI.
-//       })
-//       .catch((error) => {
-//         // Auto-play was prevented
-//         // Show paused UI.
-//       });
-//   }
-// // const playAudio = () => { 
-// //     console.log(isPlaying);
-// //     if (isPlaying) {
-// //       audio.current.play();// Play the audio
-// //     } else {
-// //       audio.current.pause();// Pause the audio
-// //     }
-
-// //     setIsPlaying(!isPlaying); // Toggle the state to update the button text
-// //   };
-  
-
-
-//   return (
-//     <div>
-//       <audio ref={audio} >
-//         <source src={"http://stream1.slowradio.com"} type="audio/mp3" />
-//         Your browser does not support the audio element.
-//       </audio>
-//       <button onClick={playAudio}>{isPlaying ? "Play":"Pause"}</button>
-//     </div>
-//   );
-// }
 
 
 import React, { useRef, useEffect, useState } from "react";
@@ -91,8 +39,10 @@ export default function AudioPlayer(props) {
 
   const handleButtonClick = () => {
     if (isPlaying) {
+        console.log("audio paused")
       pauseAudio();
     } else {
+        console.log("audio playing")
       playAudio();
     }
     setIsPlaying(!isPlaying);
@@ -101,7 +51,7 @@ export default function AudioPlayer(props) {
   return (
     <div>
       <audio ref={audio}>
-        <source src={"http://stream1.slowradio.com"} type="audio/mp3" />
+        <source src="http://stream1.slowradio.com" type="audio/mp3" />
         Your browser does not support the audio element.
       </audio>
       <button onClick={handleButtonClick}>{isPlaying ? "Pause" : "Play"}</button>
